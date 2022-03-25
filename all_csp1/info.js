6dc28f329b9ed98448646b0369a240d1e9a368fd
cjstegeman <cjstegeman@gmail.com>
2022-03-25 14:00:19 +0100

    csp_all update

---
+++ all_csp1/info.js
@@ -0,0 +1,30 @@
+let aInfo = [
+    {
+        "title": "Formulier value doorgeven via id",
+        "file"  : "form_this.html",
+        "info" : [
+            "Basis formulier opbouw in HTML-table",
+            "Gebruik van &lt;label&gt; bij &lt;input&gt;-veld",
+            "gebruik van placeholder",
+            "Javascript event: onblur, onkeyup e.a. waarbij function aangeroepen wordt",
+            "doorgeven van &lt;input&gt;-value via id van input"
+        ],
+        "oefeningen" : [
+            
+        ]
+    },
+    {
+        "title": "Formulier value doorgeven via <strong>this</strong>",
+        "file": "form_this.html",
+        "info": [
+            "Basis formulier opbouw in HTML-table",
+            "Gebruik van &lt;label&gt; bij &lt;input&gt;-veld",
+            "gebruik van placeholder",
+            "Javascript event: onblur, onkeyup e.a. waarbij function aangeroepen wordt",
+            "doorgeven van &lt;input&gt;-value via id van input"
+        ],
+        "oefeningen": [
+
+        ]
+    }
+]
---
+++ all_csp2/info.js
@@ -0,0 +1,89 @@
+
+    let aInfo = [
+        {
+            "title": "Axios-bier",
+            "file"  : "axios_beer.html",
+            "info" : [
+                "weergave van naam en brouwer",
+                "ophalen bieren via Axios",
+                "opbouw html var met JS",
+                "vullen van output-div via <i>innerHTML</i>"
+            ],
+            "oefeningen" : [
+                "Geef de velden in een tabel weer",
+                "Voeg zelf in JS of in de HTML, statisch de headers (&lt;th&gt;) toe",
+                "kijk of je de for-each loop kan vervangen voor een gewone for-loop. Welke heeft jou voorkeur?",
+                "zorg dat de nummering niet bij 0, maar bij 1 begint"
+            ]
+        },
+        {
+            "title": "Axios-bier-headers ",
+            "file": "axios_beer_headers.html",
+            "info": [
+                "ophalen de veldnamen van de biertabel",
+                "ophalen bieren via Axios",
+                "weergeven van veldnamen en biertjes via <i>innerHTML</i>"
+            ],
+            "oefeningen": [
+                "Geef velden in een tabel weer",
+                "Voeg de veldnamen als headers (&lt;th&gt;) toe",
+            ]
+        },
+        {
+            "title": "Axios-bier-brouwers ",
+            "file": "axios_beer_brouwers.html",
+            "info": [
+                "na ophalen van de biertabel, unieke array met brouwers opbouwen",
+                "weergeven van alle brouwers"
+            ],
+            "oefeningen": [
+                "sorteer de array met brouwers alfabetisch",
+                "breid je script uit: maak een array met objecten:",
+                "aBrouwers[0] = <br>"+
+                "{<br>"+
+                "&nbsp; &nbsp; \"brouwer\" = \"brouwernaam\", <br>"+
+                "&nbsp; &nbsp; \"biertjes\" = \"array_met_alle_biertjes_van_die_brouwer\", <br>"+
+                "}",
+            ]
+        },
+
+        /*
+        
+        "tabel_obj": [
+            "weergave van complete biertabel via objectopbouw van tabel met plain JS",
+            "ophalen bier-tabel via Axios",
+            "headers: haalt veldnamen uit het eerste bier-row",
+            "opbouw objecten: via document.createElement",
+            "aan element <i>table</i> append je element <i>tr</i>, daaraan een <i>td</i>, etc",
+            "aan een <i>td</i> append je een <i>textNode</i> met de html-inhoud",
+            "vullen van output-div via <i>append van table aan output-div</i>"
+        ],
+        "timer": [
+            "zelfde weergave als die van \"plain js\" maar nu met een timer op de verschillende functies"
+        ],
+        "table_flat": [
+            "json object als variabele in script gedefinieerd",
+            "\"platte\" tabel / JSON =&gt; array van studenten zonder sub-arrays",
+            "het is veel werk om deze platte tabel weer te geven als nested list van klas =&gt; student",
+            "makkelijker dan is: zet json object om in gewenste nesting"
+        ],
+        "table_nested": [
+            "json object als variabele in script gedefinieerd",
+            "\"geneste\" tabel / JSON =&gt; array van groepen met daarin sub-arrays van studenten",
+            "geneste JSON-structuur is makkelijk te vertalen naar geneste HTML (bijv. een <ul>)"
+        ],
+        "flat2nestedA": [
+            "voor weergave van geneste ul-structuur vanuit platte JSON:",
+            "- zet platte json studenten om naar geneste: groep =&gt; student",
+            "- geef geneste JSON gemakkelijk weer als HTML"
+        ],
+        "flat2nestedB": [
+            "Als nestedB maar dan 1 stap dieper nog:",
+            "- geen: groep =&gt; student",
+            "- maar: groep =&gt; student =&gt; fav"
+        ]
+    },
+    */
+
+
+]
